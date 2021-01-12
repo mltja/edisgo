09472f74ddf514bbb5001acf0a4a7ce4203f86f5
Kilian Helfenbein <s0554994@htw-berlin.de>
2021-01-12 10:36:49 +0100

    added multiprocessing functionality

--- run_eDisGo.py
+++ run_eDisGo.py
@@ -1,22 +1,51 @@
 import gc
+import os.path
 import edisgo.io.simBEV_import as sB
+import multiprocessing
 
 from pathlib import Path
 
 gc.collect()
 
-data_dir = Path( # TODO: set dir
-    # r"\\FS01\Daten_flexibel_02\simbev_results\Electrification_2050_simbev_run\standing_times_looped",
-    r"/home/local/RL-INSTITUT/kilian.helfenbein/RLI_simulation_results/simbev_results/Electrification_2050_simbev_run/standing_times_looped",
-)
+num_threads = 1
+
+# TODO: set dir
+data_dir = r"\\FS01\Daten_flexibel_02\simbev_results"
+# data_dir = r"/home/local/RL-INSTITUT/kilian.helfenbein/RLI_simulation_results/simbev_results/"
 
 localiser_data_dir = Path( # TODO: set dir
-    # r"\\FS01\Daten_flexibel_02\simbev_results\Beispieldaten",
-    r"/home/local/RL-INSTITUT/kilian.helfenbein/RLI_simulation_results/simbev_results/Beispieldaten",
+    r"\\FS01\Daten_flexibel_02\simbev_results\Beispieldaten",
+    # r"/home/local/RL-INSTITUT/kilian.helfenbein/RLI_simulation_results/simbev_results/Beispieldaten",
 )
 
+scenarios = [
+    "Electrification_2050_simbev_run",
+    "Electrification_2050_sensitivity_low_work_simbev_run",
+    "Mobility_Transition_2050_simbev_run",
+    "Szenarette_Kleinwagen_2050_simbev_run",
+    "Reference_2050_simbev_run",
+    "NEP_C_2035_simbev_run",
+]
+
+sub_dir = "standing_times_looped"
+
+data_dirs = [
+    Path(os.path.join(data_dir, scenario, sub_dir)) for scenario in scenarios
+]
+
+data_tuples = [
+    (directory, localiser_data_dir) for directory in data_dirs
+]
+
 if __name__ == "__main__":
-    sB.run_simBEV_import(
-        data_dir,
-        localiser_data_dir,
-    )
+    if num_threads == 1:
+        sB.run_simBEV_import(
+            data_dirs[0],
+            localiser_data_dir,
+        )
+    else:
+        with multiprocessing.Pool(num_threads) as pool:
+            pool.starmap(
+                sB.run_simBEV_import,
+                data_tuples,
+            )
