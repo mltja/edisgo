3ff85fbaed10f40b646327230938af2ac1273ef8
Kilian Helfenbein <s0554994@htw-berlin.de>
2021-01-18 14:15:20 +0100

    repair server run

--- run_simBEV_import.py
+++ run_simBEV_import.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 gc.collect()
 
-num_threads = 6
+num_threads = 2
 
 data_dir = Path( # TODO: set dir
     # r"\\192.168.10.221\Daten_flexibel_02\simbev_results",
@@ -31,7 +31,7 @@ scenarios = [
 sub_dir = "standing_times_looped"
 
 data_dirs = [
-    Path(os.path.join(data_dir, scenario, sub_dir)) for scenario in scenarios
+    Path(os.path.join(data_dir, scenario, sub_dir)) for scenario in scenarios[:2]
 ]
 
 data_tuples = [
