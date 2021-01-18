15b1c138e38686e0e822e3181002631241ccfa67
Kilian Helfenbein <s0554994@htw-berlin.de>
2021-01-18 09:45:31 +0100

    setup server run distribution

--- run_charging_ev.py
+++ run_charging_ev.py
@@ -14,7 +14,7 @@ data_dir = r"\\FS01\Daten_flexibel_02\simbev_results"
 # data_dir = r"/home/local/RL-INSTITUT/kilian.helfenbein/RLI_simulation_results/simbev_results"
 
 scenarios = [
-    "FEHLERHAFT_Electrification_2050_simbev_run", # TODO: switch
+    "Electrification_2050_simbev_run",
     "Electrification_2050_sensitivity_low_work_simbev_run",
     "Mobility_Transition_2050_simbev_run",
     "Szenarette_Kleinwagen_2050_simbev_run",
