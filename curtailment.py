50df64bc2d8e6d4a7b1be12b06e5dc4397de928a
Kilian Helfenbein <s0554994@htw-berlin.de>
2021-02-21 17:20:56 +0100

    test

--- curtailment.py
+++ curtailment.py
@@ -899,8 +899,6 @@ def calculate_curtailment(
         day,
 ):
     try:
-        os.system("taskset -p 0xff %d" % os.getpid())
-
         mv_grid_id = int(grid_dir.parts[-2])
         scenario = grid_dir.parts[-3]
 
