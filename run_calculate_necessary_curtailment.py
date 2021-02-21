d9b4dd60263df0717a3594508762ce4f5b5ed6c7
Kilian Helfenbein <s0554994@htw-berlin.de>
2021-02-21 13:15:35 +0100

    daywise test

--- run_calculate_necessary_curtailment.py
+++ run_calculate_necessary_curtailment.py
@@ -108,7 +108,7 @@ def run_calculate_curtailment(
             else:
                 num_threads = 2
 
-            num_threads = min(num_threads, len(days), 2) # TODO
+            num_threads = min(num_threads, len(days), 3) # TODO
 
             data_tuples = [
                 (directory, day, ts_count)
