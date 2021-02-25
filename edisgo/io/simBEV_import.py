4646211280df31526755c6386d6f705e03ef5d3b
Kilian Helfenbein <s0554994@htw-berlin.de>
2021-02-25 13:17:53 +0100

    work uses 50% of public now

--- edisgo/io/simBEV_import.py
+++ edisgo/io/simBEV_import.py
@@ -451,7 +451,7 @@ def get_charging_points(
 
         # add some extra charging locations at work, because of a low number of charging stations
         df_append = cp_dfs[1].copy().sample(
-            n=int(np.ceil(0.3 * len(cp_dfs[1]))),
+            n=int(np.ceil(0.5 * len(cp_dfs[1]))),
             random_state=25588,
         )
 
@@ -467,7 +467,7 @@ def get_charging_points(
             else:
                 cp_dfs[3] = cp_dfs[1].copy()
 
-        sum_weight_target = cp_dfs[3].weight.sum() / 0.7 * 0.3
+        sum_weight_target = cp_dfs[3].weight.sum()# / 0.7 * 0.3
         sum_weight = df_append.weight.sum()
         df_append.weight = df_append.weight.multiply(sum_weight_target / sum_weight)
 
