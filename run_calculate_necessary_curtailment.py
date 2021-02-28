2d263f1910b1cc5066226e1670451b72e8a3c281
Kilian Helfenbein <s0554994@htw-berlin.de>
2021-02-28 10:18:51 +0100

    setup server run

--- run_calculate_necessary_curtailment.py
+++ run_calculate_necessary_curtailment.py
@@ -24,29 +24,29 @@ from edisgo.edisgo import import_edisgo_from_files
 
 gc.collect()
 
-# os.sched_setaffinity(0,range(1000)) # TODO
+os.sched_setaffinity(0,range(1000)) # TODO
 
-num_threads = 1 # TODO
+num_threads = 10 # TODO
 
 data_dir = Path( # TODO: set dir
-    r"\\192.168.10.221\Daten_flexibel_02\simbev_results",
-    # r"/home/local/RL-INSTITUT/kilian.helfenbein/RLI_simulation_results/simbev_results",
+    # r"\\192.168.10.221\Daten_flexibel_02\simbev_results",
+    r"/home/local/RL-INSTITUT/kilian.helfenbein/RLI_simulation_results/simbev_results",
 )
 
 sub_dir = r"eDisGo_curtailment_results" # TODO
 
 scenarios = [ # TODO
-    # "NEP_C_2035",
-    # "Reference_2050",
-    # "Szenarette_Kleinwagen_2050",
+    "NEP_C_2035",
+    "Reference_2050",
+    "Szenarette_Kleinwagen_2050",
     "Mobility_Transition_2050",
-    # "Electrification_2050",
-    # "Electrification_2050_sensitivity_low_work",
+    "Electrification_2050",
+    "Electrification_2050_sensitivity_low_work",
 ]
 
-grid_ids = ["2534"]#, "177", "1056", "1690", "1811", "176"] # TODO
+grid_ids = ["2534", "177", "1056", "1690", "1811", "176"] # TODO
 
-strategies = ["dumb"]#, "grouped", "reduced", "residual"] # TODO
+strategies = ["dumb", "grouped", "reduced", "residual"] # TODO
 
 data_dirs = [
     Path(os.path.join(data_dir, sub_dir, scenario, grid_id, strategy))
@@ -66,7 +66,7 @@ def run_calculate_curtailment(
 
         print("Scenario {} with strategy {} in grid {} is being processed.".format(scenario, strategy, grid_id))
 
-        mode = "years" # TODO
+        mode = "weeks" # TODO
 
         if mode == "days":
             ts_count = 96
