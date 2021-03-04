d091d4fc21842344bed70c62974675ad74b708c3
Kilian Helfenbein <s0554994@htw-berlin.de>
2021-03-04 16:54:09 +0100

    2534

--- edisgo/tools/plots.py
+++ edisgo/tools/plots.py
@@ -651,7 +651,7 @@ def mv_grid_topology(
     else:
         if kwargs.get("bus_colors", None):
             bus_colors = pd.Series(kwargs.get("bus_colors")).loc[
-                pypsa_plot.buses]
+                pypsa_plot.buses.index]
         else:
             logging.warning(
                 "Choice for `node_color` is not valid. Default bus colors are "
@@ -660,7 +660,7 @@ def mv_grid_topology(
             bus_colors = "r"
         if kwargs.get("bus_sizes", None):
             bus_sizes = pd.Series(kwargs.get("bus_sizes")).loc[
-                pypsa_plot.buses]
+                pypsa_plot.buses.index]
         else:
             logging.warning(
                 "Choice for `node_color` is not valid. Default bus sizes are "
