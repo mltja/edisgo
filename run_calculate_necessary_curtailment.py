e29d6a109bcfdeee0e511f19b647ee6609bfef32
Kilian Helfenbein <s0554994@htw-berlin.de>
2021-03-04 07:57:03 +0100

    test 176

--- run_calculate_necessary_curtailment.py
+++ run_calculate_necessary_curtailment.py
@@ -26,7 +26,7 @@ gc.collect()
 
 os.sched_setaffinity(0,range(1000)) # TODO
 
-num_threads = 12 # TODO
+num_threads = 1 # TODO
 
 data_dir = Path( # TODO: set dir
     # r"\\192.168.10.221\Daten_flexibel_02\simbev_results",
@@ -37,16 +37,16 @@ sub_dir = r"eDisGo_curtailment_results" # TODO
 
 scenarios = [ # TODO
     "NEP_C_2035",
-    "Reference_2050",
-    "Szenarette_Kleinwagen_2050",
-    "Mobility_Transition_2050",
-    "Electrification_2050",
-    "Electrification_2050_sensitivity_low_work",
+    # "Reference_2050",
+    # "Szenarette_Kleinwagen_2050",
+    # "Mobility_Transition_2050",
+    # "Electrification_2050",
+    # "Electrification_2050_sensitivity_low_work",
 ]
 
-grid_ids = ["177"]#["2534", "177", "1056", "1690", "1811", "176"] # TODO
+grid_ids = ["176"]#["2534", "177", "1056", "1690", "1811", "176"] # TODO
 
-strategies = ["dumb", "grouped", "reduced", "residual"] # TODO
+strategies = ["dumb"]#, "grouped", "reduced", "residual"] # TODO
 
 data_dirs = [
     Path(os.path.join(data_dir, sub_dir, scenario, grid_id, strategy))
