f1995fcec91f5901e02d77464b60a9679a536a83
Kilian Helfenbein <s0554994@htw-berlin.de>
2021-03-07 13:55:31 +0100

    177 test

--- curtailment.py
+++ curtailment.py
@@ -1614,8 +1614,8 @@ def calculate_curtailment(
         # for col in edisgo.timeseries.charging_points_active_power.columns:
         #     edisgo.timeseries.charging_points_active_power[col].values[:] = 0
         #
-        # print("CPs:", edisgo.timeseries.charging_points_active_power.sum().sum())
-        # print("Loads:", edisgo.timeseries.loads_active_power.sum().sum())
+        print("CPs:", edisgo.timeseries.charging_points_active_power.sum().sum())
+        print("Loads:", edisgo.timeseries.loads_active_power.sum().sum())
 
         t0 = perf_counter()
 
