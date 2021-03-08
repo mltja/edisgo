ef8a7a7aec1093be7362957c71ed73fb9bf8170e
Kilian Helfenbein <s0554994@htw-berlin.de>
2021-03-08 14:33:12 +0100

    generate only mv third quarter

--- generate_edisgo_objects.py
+++ generate_edisgo_objects.py
@@ -84,7 +84,7 @@ def generate_edisgo_objects(
             export_dir = Path(
                 os.path.join(
                     data_dir,
-                    "eDisGo_curtailment_test_mv_second_half",
+                    "eDisGo_curtailment_test_mv_third_quarter",
                     scenario,
                     grid_id,
                     strategy,
