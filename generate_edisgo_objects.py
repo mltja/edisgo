6ba1adb63240f6aaa2fac674b74433d7f229ea5d
Kilian Helfenbein <s0554994@htw-berlin.de>
2021-03-08 09:04:53 +0100

    testing 177 only hvmv connections

--- generate_edisgo_objects.py
+++ generate_edisgo_objects.py
@@ -84,7 +84,7 @@ def generate_edisgo_objects(
             export_dir = Path(
                 os.path.join(
                     data_dir,
-                    "eDisGo_curtailment_test_mv",
+                    "eDisGo_curtailment_test_hvmv",
                     scenario,
                     grid_id,
                     strategy,
