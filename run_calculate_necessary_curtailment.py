9db3db91e4a802a0dca70bd0a629c822eff34362
Kilian Helfenbein <s0554994@htw-berlin.de>
2021-03-18 21:24:45 +0100

    calc curtailment 1690 + 1811

--- run_calculate_necessary_curtailment.py
+++ run_calculate_necessary_curtailment.py
@@ -26,7 +26,7 @@ gc.collect()
 
 os.sched_setaffinity(0,range(1000)) # TODO
 
-num_threads = 16 # TODO
+num_threads = 11 # TODO
 
 data_dir = Path( # TODO: set dir
     # r"\\192.168.10.221\Daten_flexibel_02\simbev_results",
@@ -45,7 +45,7 @@ scenarios = [ # TODO
 # "Szenarette_Kleinwagen_2050",
 # "Mobility_Transition_2050",
 
-grid_ids = ["1056"]#["2534", "177", "1056", "1690", "1811", "176"] # TODO
+grid_ids = ["1690", "1811"]#["2534", "177", "1056", "1690", "1811", "176"] # TODO
 
 strategies = ["dumb", "grouped", "reduced", "residual"] # TODO
 
