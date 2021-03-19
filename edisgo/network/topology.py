45f9add7a1a0c9bfa5ce15475555a0403887c6ad
Kilian Helfenbein <s0554994@htw-berlin.de>
2021-03-19 11:05:11 +0100

    testing edisgo object generation

--- edisgo/network/topology.py
+++ edisgo/network/topology.py
@@ -1604,15 +1604,15 @@ class Topology:
                 if "virtual" not in dist_min_obj["repr"]:
                     # # FIXME: Workaround Kilian PF problems with integration of CPs
                     # if "charging" not in dist_min_obj["repr"].lower():
-                    if dist_min_obj["shp"].geom_type == "Point":
-                        target_obj_result = self._connect_mv_bus_to_target_object(
-                            edisgo_object=edisgo_object,
-                            bus=self.buses_df.loc[bus, :],
-                            target_obj=dist_min_obj,
-                        )
+                    # if dist_min_obj["shp"].geom_type == "Point":
+                    target_obj_result = self._connect_mv_bus_to_target_object(
+                        edisgo_object=edisgo_object,
+                        bus=self.buses_df.loc[bus, :],
+                        target_obj=dist_min_obj,
+                    )
 
-                        if target_obj_result is not None:
-                            comp_connected = True
+                    if target_obj_result is not None:
+                        comp_connected = True
                             break
 
             if not comp_connected:
