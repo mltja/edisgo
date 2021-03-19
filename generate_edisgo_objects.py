9afbed7ae852d1e33116143b98d9859ac4c5f2c3
Kilian Helfenbein <s0554994@htw-berlin.de>
2021-03-19 08:12:30 +0100

    test generate edisgo objects v5 only voltage level 4

--- generate_edisgo_objects.py
+++ generate_edisgo_objects.py
@@ -122,7 +122,7 @@ def generate_edisgo_objects(
             export_dir = Path(
                 os.path.join(
                     data_dir,
-                    "eDisGo_object_files_v5 ", # TODO
+                    "eDisGo_object_files_v5", # TODO
                     scenario,
                     grid_id,
                     strategy,
