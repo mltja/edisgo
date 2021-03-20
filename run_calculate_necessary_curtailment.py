626c450bc8365a8fda1c220f8d263635fa081083
Kilian Helfenbein <s0554994@htw-berlin.de>
2021-03-20 23:24:22 +0100

    calc curtailment only lv reinforcement

--- run_calculate_necessary_curtailment.py
+++ run_calculate_necessary_curtailment.py
@@ -24,30 +24,30 @@ from edisgo.edisgo import import_edisgo_from_files
 
 gc.collect()
 
-# os.sched_setaffinity(0,range(1000)) # TODO
+os.sched_setaffinity(0,range(1000)) # TODO
 
-num_threads = 1 # TODO
+num_threads = 13 # TODO
 
 data_dir = Path( # TODO: set dir
     r"\\192.168.10.221\Daten_flexibel_02\simbev_results",
     # r"/home/local/RL-INSTITUT/kilian.helfenbein/RLI_simulation_results/simbev_results",
 )
 
-sub_dir = r"eDisGo_object_files" # TODO
+sub_dir = r"eDisGo_object_files_only_lv_reinforcement" # TODO
 
 scenarios = [ # TODO
-    # "NEP_C_2035",
-    # "Reference_2050",
+    "NEP_C_2035",
+    "Reference_2050",
     "Electrification_2050",
-    # "Electrification_2050_sensitivity_low_work",
+    "Electrification_2050_sensitivity_low_work",
 ]
 
 # "Szenarette_Kleinwagen_2050",
 # "Mobility_Transition_2050",
 
-grid_ids = ["1811"]#["2534", "177", "1056", "1690", "1811", "176"] # TODO
+grid_ids = ["1056", "1690", "1811"]#["2534", "177", "1056", "1690", "1811", "176"] # TODO
 
-strategies = ["dumb"]#, "grouped", "reduced", "residual"] # TODO
+strategies = ["dumb", "grouped", "reduced", "residual"] # TODO
 
 data_dirs = [
     Path(os.path.join(data_dir, sub_dir, scenario, grid_id, strategy))
@@ -84,7 +84,7 @@ def run_calculate_curtailment(
         else:
             days = [None]
 
-        for day in [days[1]]: # TODO
+        for day in days: # TODO
             stepwise_curtailment(
                 directory,
                 day,
