1bfcb7a7f41924195ffacef491757ba252bf4011
Kilian Helfenbein <s0554994@htw-berlin.de>
2021-03-22 09:02:55 +0100

    calc cur 1056

--- run_calculate_necessary_curtailment.py
+++ run_calculate_necessary_curtailment.py
@@ -45,7 +45,7 @@ scenarios = [ # TODO
 # "Szenarette_Kleinwagen_2050",
 # "Mobility_Transition_2050",
 
-grid_ids = ["1690"]#["2534", "177", "1056", "1690", "1811", "176"] # TODO
+grid_ids = ["1056"]#["2534", "177", "1056", "1690", "1811", "176"] # TODO
 
 strategies = ["dumb", "grouped", "reduced", "residual"] # TODO
 
