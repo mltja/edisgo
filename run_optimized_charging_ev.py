a61102ad3dcd707b4fabf802c47a64a7d8061bc1
Anya Heider <50416149+AnyaHe@users.noreply.github.com>
2021-05-06 08:42:05 +0200

    introduce iterative approach of downstream node matrix

--- run_optimized_charging_ev.py
+++ run_optimized_charging_ev.py
@@ -1,7 +1,7 @@
 # Test to implement functionality of optimisation
 from edisgo.edisgo import import_edisgo_from_files
 from edisgo.flex_opt.optimization import setup_model, update_model, optimize
-from edisgo.tools.networkx_helper import get_downstream_nodes_matrix
+from edisgo.tools.networkx_helper import get_downstream_nodes_matrix, get_downstream_nodes_matrix_iterative
 import pyomo.environ as pm
 from pyomo.opt import SolverStatus, TerminationCondition
 import numpy as np
@@ -12,6 +12,15 @@ import seaborn as sns
 import geopandas as gpd
 sns.set()
 
+
+
+# grid_dir = 'C:/Users/Anya.Heider/DistFlex/AllgaeuNetz/data'
+# edisgo = import_edisgo_from_files(grid_dir)
+# get_downstream_nodes_matrix_iterative(edisgo)
+
+downstream_nodes_matrix = pd.read_csv('grid_data/downstream_node_matrix.csv',
+                                      index_col=0)
+
 grid_id = 1056
 
 edisgo_dir = r'\\192.168.10.221\Daten_flexibel_02\simbev_results\eDisGo_object_files_final\Electrification_2050\{}\reduced'.format(grid_id)#Todo: change back to final in the end
@@ -19,23 +28,23 @@ edisgo_obj = import_edisgo_from_files(edisgo_dir, import_timeseries=True)
 
 print('eDisGo object imported.')
 
-downstream_nodes_matrix = get_downstream_nodes_matrix(edisgo_obj)
+downstream_nodes_matrix = get_downstream_nodes_matrix_iterative(edisgo_obj)
 downstream_nodes_matrix.to_csv('grid_data/downstream_node_matrix.csv')
 
-flexibility_bands_home = \
-    pd.read_csv('grid_data/ev_flexibility_bands_home.csv', index_col=0)
-flexibility_bands_work = \
-    pd.read_csv('grid_data/ev_flexibility_bands_work.csv', index_col=0)
-flexibility_bands = pd.concat([flexibility_bands_work, flexibility_bands_home],
-                              axis=1)
-cp_mapping_dir = r'\\192.168.10.221\Daten_flexibel_02\simbev_results\Electrification_2050_simbev_run\eDisGo_charging_time_series\{}'.format(grid_id)
-mapping_home = \
-    gpd.read_file(cp_mapping_dir + '\cp_data_home_within_grid_{}.geojson'.
-                  format(grid_id)).set_index('edisgo_id')
-mapping_work = \
-    gpd.read_file(cp_mapping_dir + '\cp_data_work_within_grid_{}.geojson'.
-                  format(grid_id)).set_index('edisgo_id')
-mapping = pd.concat([mapping_work, mapping_home])
+# flexibility_bands_home = \
+#     pd.read_csv('grid_data/ev_flexibility_bands_home.csv', index_col=0)
+# flexibility_bands_work = \
+#     pd.read_csv('grid_data/ev_flexibility_bands_work.csv', index_col=0)
+# flexibility_bands = pd.concat([flexibility_bands_work, flexibility_bands_home],
+#                               axis=1)
+# cp_mapping_dir = r'\\192.168.10.221\Daten_flexibel_02\simbev_results\Electrification_2050_simbev_run\eDisGo_charging_time_series\{}'.format(grid_id)
+# mapping_home = \
+#     gpd.read_file(cp_mapping_dir + '\cp_data_home_within_grid_{}.geojson'.
+#                   format(grid_id)).set_index('edisgo_id')
+# mapping_work = \
+#     gpd.read_file(cp_mapping_dir + '\cp_data_work_within_grid_{}.geojson'.
+#                   format(grid_id)).set_index('edisgo_id')
+# mapping = pd.concat([mapping_work, mapping_home])
 
 # timesteps_per_week = 672
 # for week in range(int(len(edisgo_obj.timeseries.timeindex)/timesteps_per_week)-1):#edisgo_obj.timeseries.timeindex.week.unique()
