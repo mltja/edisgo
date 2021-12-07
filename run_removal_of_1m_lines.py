d8c406bc4b2a143057498ca5d79fd346a5c02096
aheider <anya.heider@rl-institut.de>
2021-12-07 18:15:19 +0100

    adapt removal of 1m lines to new format, make sure that switches are closed

--- run_removal_of_1m_lines.py
+++ run_removal_of_1m_lines.py
@@ -1,5 +1,7 @@
 from edisgo.edisgo import import_edisgo_from_files
+from edisgo.network.components import Switch
 import multiprocessing as mp
+
 
 def remove_1m_end_line(edisgo, line):
     # Check for end buses
@@ -17,14 +19,11 @@ def remove_1m_end_line(edisgo, line):
     # Move connected elements of end bus to the other bus
     connected_elements = edisgo.topology.get_connected_components_from_bus(line[end_bus])
     # move elements to neighboring bus
-    rename_dict = {line[end_bus]:line[neighbor_bus]}
+    rename_dict = {line[end_bus]: line[neighbor_bus]}
     for Type, components in connected_elements.items():
-        if not components.empty and Type != 'Line':
-            if Type == 'Switch':
-                edisgo.topology.switches_df = edisgo.topology.switches_df.replace(rename_dict)
-            else:
-                setattr(edisgo.topology, Type.lower() + 's_df',
-                        getattr(edisgo.topology, Type.lower() + 's_df').replace(
+        if not components.empty and Type != 'lines':
+            setattr(edisgo.topology, Type.lower() + '_df',
+                        getattr(edisgo.topology, Type.lower() + '_df').replace(
                             rename_dict))
     # remove line
     edisgo.topology.remove_line(line.name)
@@ -33,23 +32,36 @@ def remove_1m_end_line(edisgo, line):
 
 # For removal on 1m lines
 def remove_1m_lines_from_edisgo(grid_id):
-    try:
-        print('Removing 1m lines for grid {}'.format(grid_id))
-        edisgo = import_edisgo_from_files(r'U:\Software\eDisGo_object_files\simbev_nep_2035_results\{}\reduced'.format(grid_id))
+    # try:
+    print('Removing 1m lines for grid {}'.format(grid_id))
+    edisgo = import_edisgo_from_files(r'U:\Software\eDisGo_object_files\simbev_nep_2035_results\{}\reduced'.format(grid_id))
 
-        lines = edisgo.topology.lines_df.loc[edisgo.topology.lines_df.length == 0.001]
-        for name, line in lines.iterrows():
-            remove_1m_end_line(edisgo, line)
-        edisgo.topology.to_csv(r'U:\Software\eDisGo_object_files\simbev_nep_2035_results\{}\reduced\topology'.format(grid_id))
-    except Exception as e:
-        print('Problem in grid {}.'.format(grid_id))
-        print(e)
+    # close switches such that lines with connected switches are not removed
+    switches = [Switch(id=_, topology=edisgo.topology)
+                for _ in edisgo.topology.switches_df.index]
+    switch_status = {}
+    for switch in switches:
+        switch_status[switch] = switch.state
+        switch.close()
+    # get all lines and remove end 1m lines
+    lines = edisgo.topology.lines_df.loc[edisgo.topology.lines_df.length == 0.001]
+    for name, line in lines.iterrows():
+        remove_1m_end_line(edisgo, line)
+    # set switches back to original state
+    for switch in switches:
+        if switch_status[switch] == 'open':
+            switch.open()
+    edisgo.topology.to_csv(r'U:\Software\eDisGo_object_files\simbev_nep_2035_results\{}\reduced\topology'.format(grid_id))
+    # except Exception as e:
+    #     print('Problem in grid {}.'.format(grid_id))
+    #     print(e)
 
 
 if __name__ == '__main__':
-    pool = mp.Pool(3)
-
-    grid_ids = [1056, 1811, 2534]
-    pool.map(remove_1m_lines_from_edisgo, grid_ids)
-    pool.close()
+    # pool = mp.Pool(3)
+    #
+    # grid_ids = [1056, 1811, 2534]
+    # pool.map(remove_1m_lines_from_edisgo, grid_ids)
+    # pool.close()
+    remove_1m_lines_from_edisgo(2534)
     print('SUCCESS')
